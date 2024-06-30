2a79a781437fae4626ddeff39ba050fedcd152ef
rockyx2000 <hametu1992@gmail.com>
2024-06-30 16:48:58 +0900

    create-post

--- gaming_clipapp/app/controllers/posts_controller.rb
+++ gaming_clipapp/app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def index
+   # @new_post = Clip.new
+  end
+
+  def new_posts
+    @new_post = Clip.new
+  end
+
+  def create_posts
+    @new_post = Clip.new(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:clip).permit(:title, :description, :video)
   end
 end
